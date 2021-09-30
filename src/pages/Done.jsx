bc50eead3f65fd3b3303fb91c4916cf5ee17e1a5
junglejf <jfcchagas@id.uff.br>
2021-09-30 18:35:34 -0300

    Antes de iniciar o merge

--- src/pages/Done.jsx
+++ src/pages/Done.jsx
@@ -1,62 +1,62 @@
-// React
-import React from 'react';
-import { Link } from 'react-router-dom';
+// // React
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-// Bootstrap
-import { Button, Modal } from 'react-bootstrap';
+// // Bootstrap
+// import { Button, Modal } from 'react-bootstrap';
 
-// Children
-import copy from 'clipboard-copy';
-import Header from '../components/Header';
+// // Children
+// import copy from 'clipboard-copy';
+// import Header from '../components/Header';
 
-// Services
+// // Services
 
-// Helpers
-import { loadLocalStorage } from '../helpers/localStorageHelper';
+// // Helpers
+// import { loadLocalStorage } from '../helpers/localStorageHelper';
 
-// Images
-import shareIcon from '../images/shareIcon.svg';
+// // Images
+// import shareIcon from '../images/shareIcon.svg';
 
-function Done() {
-  const [showModal, setShowModal] = useState(false); // Mostrar mensagem
-  const handleShowModal = () => setShowModal(true);
-  const handleCloseModal = () => setShowModal(false);
-  const [favorites, setFavorites] = useState(loadLocalStorage('favoriteRecipes'));
-  const [filterBtns, setFilterBtns] = useState('all');
+// function Done() {
+//   const [showModal, setShowModal] = useState(false); // Mostrar mensagem
+//   const handleShowModal = () => setShowModal(true);
+//   const handleCloseModal = () => setShowModal(false);
+//   const [doneRecipes, setDoneRecipes] = useState(loadLocalStorage('doneRecipes'));
+//   const [filterBtns, setFilterBtns] = useState('all');
 
-  function filterByType() {
-    const filtering = favorites.filter((favorite) => favorite.type === filterBtns);
-    return filtering;
-  }
+//   function filterByType() { // talvez vire uam funcao helper, pois está sendo usada em Favorites
+//     const filtering = doneRecipes.filter((recipe) => recipe.type === filterBtns);
+//     return filtering;
+//   }
 
-  return (
-    <section>
-      <Header title="Receitas Feitas" />
-      <section>
-        <Button
-          variant="dark"
-          data-testid="filter-by-all-btn"
-          onClick={ () => setFilterBtns('all') }
-        >
-          All
-        </Button>
-        <Button
-          variant="danger"
-          data-testid="filter-by-food-btn"
-          onClick={ () => setFilterBtns('comida') }
-        >
-          Food
-        </Button>
-        <Button
-          variant="success"
-          data-testid="filter-by-drink-btn"
-          onClick={ () => setFilterBtns('bebida') }
-        >
-          Drinks
-        </Button>
-      </section>
-    </section>
-  );
-}
+//   return (
+//     <section>
+//       <Header title="Receitas Feitas" />
+//       <section>
+//         <Button
+//           variant="dark"
+//           data-testid="filter-by-all-btn"
+//           onClick={ () => setFilterBtns('all') }
+//         >
+//           All
+//         </Button>
+//         <Button
+//           variant="danger"
+//           data-testid="filter-by-food-btn"
+//           onClick={ () => setFilterBtns('comida') }
+//         >
+//           Food
+//         </Button>
+//         <Button
+//           variant="success"
+//           data-testid="filter-by-drink-btn"
+//           onClick={ () => setFilterBtns('bebida') }
+//         >
+//           Drinks
+//         </Button>
+//       </section>
+//     </section>
+//   );
+// }
 
-export default Done;
+// export default Done;
